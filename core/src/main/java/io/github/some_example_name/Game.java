a6dbc664b62f4555c03225e6154b4e454a00ca24
dosu199 <smhuic@gmail.com>
2025-07-29 17:29:51 +0200

    Added Game class

--- core/src/main/java/io/github/some_example_name/Game.java
+++ core/src/main/java/io/github/some_example_name/Game.java
@@ -11,46 +11,20 @@ import com.badlogic.gdx.utils.ScreenUtils;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import org.w3c.dom.Text;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class Main extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture player;
-    private Texture enemy;
-    private BitmapFont font;
-    private FitViewport viewport;
+public class Game extends ApplicationAdapter {
     private final Vector2 mousePosition = new Vector2();
+    private FitViewport viewport;
+    private SpriteBatch batch;
+    private Player player;
+    private Enemy enemy;
+    private BitmapFont font;
+    private EntityManager entityMgr;
 
-    @Override
-    public void create() {
-        batch = new SpriteBatch();
-        player = new Texture("player.png");
-        enemy = new Texture("enemy.png");
-        font = new BitmapFont();
-        font.getData().setLineHeight(1);
-        viewport =  new FitViewport(800, 500);
+    public Game() {
+        this.entityMgr = new EntityManager();
     }
 
-    @Override
-    public void render() {
-        processInput();
-        step();
-        draw();
-    }
-
-    @Override
-    public void resize(int width, int height) {
-        // Resizing will screw up our logic
-        viewport.update(width, height, true);
-    }
-
-    @Override
-    public void dispose() {
-        batch.dispose();
-        player.dispose();
-        enemy.dispose();
-    }
-
-    private void processInput() {
+    public void processInput() {
         mousePosition.set(0.0f, 0.0f);
         if (Gdx.input.isTouched()) {
             mousePosition.set(Gdx.input.getX(), Gdx.input.getY()); // Get where the touch happened on screen
@@ -58,21 +32,45 @@ public class Main extends ApplicationAdapter {
         }
     }
 
-    private void step() {
-        // Update game logic here
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        font = new BitmapFont();
+        font.getData().setLineHeight(1);
+        viewport = new FitViewport(800, 500);
+    }
+//
+//    public void update() {
+//        this.entityMgr.getEntities().forEach(entity -> {
+//            entity.update();
+//            entity.render();
+//        });
+//    }
+
+    @Override
+    public void render() {
+        processInput();
+//        step();
+        draw();
     }
 
-    private void draw() {
+    @Override
+    public void dispose() {
+        batch.dispose();
+        font.dispose();
+    }
+
+
+    public void draw() {
         ScreenUtils.clear(Color.BLACK);
         viewport.apply();
         batch.setProjectionMatrix(viewport.getCamera().combined);
 
         System.out.println("Position: (" + mousePosition.x + ", " + mousePosition.y + ")");
-
+        System.out.println(entityMgr.getPlayer().getPlayerTexture());
         batch.begin();
-
-        batch.draw(player, 100, 100, 100, 100);
-        batch.draw(enemy, 600, 100, 100, 100);
+        batch.draw(entityMgr.getPlayer().getPlayerTexture(), 100, 100, 100, 100);
+        batch.draw(entityMgr.getEnemy().getEnemyTexture(), 600, 100, 100, 100);
 
         // This is game logic and does not belong here
         if (mousePosition.x > 600 && mousePosition.x < 700 && mousePosition.y > 100 && mousePosition.y < 200) {
