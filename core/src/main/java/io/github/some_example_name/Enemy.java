a88a5b49e117d307f7b335b930098151f33e21f6
dosu199 <smhuic@gmail.com>
2025-08-01 14:12:30 +0200

    Update Enemy class draw method

--- core/src/main/java/io/github/some_example_name/Enemy.java
+++ core/src/main/java/io/github/some_example_name/Enemy.java
@@ -1,26 +1,38 @@
 package io.github.some_example_name;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class Enemy implements Entity {
     private int posX;
     private int posY;
-    private final Texture enemyTexture;
+    private final int width;
+    private final int height;
+    private Texture enemyTexture;
 
 
     public Enemy(int posX, int posY) {
         this.posX = posX;
         this.posY = posY;
-        this.enemyTexture = new Texture("enemy.png");
+        this.width = 100;
+        this.height = 100;
+    }
+
+    public Texture createTexture() {
+        this.enemyTexture = new Texture(Gdx.files.internal("enemy.png"));
+        return this.enemyTexture;
     }
 
     public Texture getEnemyTexture() {
         return enemyTexture;
     }
 
-    public void render() {
+    public void draw(SpriteBatch batch) {
+        batch.draw(enemyTexture, posX, posY, width, height);
 
     }
+
     public void update() {
 
     }
