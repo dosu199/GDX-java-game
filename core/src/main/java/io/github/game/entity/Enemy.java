d4af800a2a7ace2fcf0ce3725d4bbed752a09a29
dosu199 <smhuic@gmail.com>
2025-08-06 11:13:26 +0200

    Enemy and Player changes

---
+++ core/src/main/java/io/github/game/entity/Enemy.java
@@ -0,0 +1,18 @@
+package io.github.game.entity;
+
+import com.badlogic.gdx.graphics.Texture;
+import io.github.game.util.V2;
+
+public class Enemy extends Entity {
+    public Enemy(Texture texture, V2 p, V2 scale) {
+        super(texture, p, scale);
+    }
+
+    public void update() {
+    }
+
+    @Override
+    public EntityType type() {
+        return EntityType.ENEMY;
+    }
+}
