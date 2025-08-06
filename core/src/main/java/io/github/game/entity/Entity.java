76092070407ab3f85ed8be5227c74305cd95acd5
dosu199 <smhuic@gmail.com>
2025-08-06 11:13:50 +0200

    Entity and EntityManager changes

--- core/src/main/java/io/github/game/entity/Entity.java
+++ core/src/main/java/io/github/game/entity/Entity.java
@@ -1,23 +1,28 @@
-package io.github.some_example_name;
+package io.github.game.entity;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import io.github.game.util.V2;
 
 public abstract class Entity {
     private V2 p;
     private V2 scale;
     private Texture texture;
 
-    public Entity(String textureLocation, V2 p, V2 scale) {
+    public enum EntityType {PLAYER, ENEMY}
+
+    public Entity(Texture texture, V2 p, V2 scale) {
         this.p = new V2(p.x, p.y);
         this.scale = scale;
-        this.texture = new Texture(Gdx.files.internal(textureLocation));
+        this.texture = texture;
     }
 
-    void draw(SpriteBatch batch) {
+    public void draw(SpriteBatch batch) {
         batch.draw(texture, p.x, p.y, scale.x, scale.y);
     }
+
+    public abstract EntityType type();
 
     void update() {
     }
--- core/src/main/java/io/github/game/entity/EntityManager.java
+++ core/src/main/java/io/github/game/entity/EntityManager.java
@@ -1,4 +1,8 @@
-package io.github.some_example_name;
+package io.github.game.entity;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import io.github.game.util.V2;
 
 import java.util.ArrayList;
 import java.util.Objects;
@@ -9,9 +13,9 @@ public class EntityManager {
 
     public EntityManager() {
         this.entities = new ArrayList<>();
-        addEntity(new Player("player.png", new V2(100.0F, 100.00F), new V2(100.0F, 100.0F)));
+        addEntity(new Player(new Texture(Gdx.files.internal("player.png")), new V2(100.0F, 100.00F), new V2(100.0F, 100.0F)));
         this.playerIndex = 0;
-        addEntity(new Enemy("enemy.png", new V2(600.0F, 100.00F), new V2(100.0F, 100.0F)));
+        addEntity(new Enemy(new Texture(Gdx.files.internal("enemy.png")), new V2(600.0F, 100.00F), new V2(100.0F, 100.0F)));
     }
 
     public void addEntity(Entity entity) {
