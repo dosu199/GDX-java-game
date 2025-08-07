a510bf692a0319c03588f4e4f8852965417c9127
dosu199 <smhuic@gmail.com>
2025-08-07 16:29:58 +0200

    Added hashMap of textures

--- core/src/main/java/io/github/game/entity/EntityManager.java
+++ core/src/main/java/io/github/game/entity/EntityManager.java
@@ -2,20 +2,27 @@ package io.github.game.entity;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import io.github.game.Enums.EntityType;
 import io.github.game.util.V2;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Objects;
 
 public class EntityManager {
     private ArrayList<Entity> entities;
     public int playerIndex = -1;
+    public HashMap<EntityType, Texture> textures;
 
     public EntityManager() {
+        textures = new HashMap<>();
+        textures.put(EntityType.PLAYER, new Texture(Gdx.files.internal("player.png")));
+        textures.put(EntityType.ENEMY, new Texture(Gdx.files.internal("enemy.png")));
+
         this.entities = new ArrayList<>();
-        addEntity(new Player(new Texture(Gdx.files.internal("player.png")), new V2(100.0F, 100.00F), new V2(100.0F, 100.0F)));
+        addEntity(new Player(textures.get(EntityType.PLAYER), new V2(400.0F, 100.00F), new V2(100.0F, 100.0F)));
         this.playerIndex = 0;
-        addEntity(new Enemy(new Texture(Gdx.files.internal("enemy.png")), new V2(600.0F, 100.00F), new V2(100.0F, 100.0F)));
+        addEntity(new Enemy(textures.get(EntityType.ENEMY), new V2(700.0F, 100.00F), new V2(100.0F, 100.0F)));
     }
 
     public void addEntity(Entity entity) {
@@ -23,12 +30,12 @@ public class EntityManager {
     }
 
     public Player getPlayer() {
-       return (Player) entities.get(playerIndex);
+        return (Player) entities.get(playerIndex);
     }
 
-//    public Enemy getEnemy() {
-//        return enemy;
-//    }
+    public HashMap<EntityType, Texture> getTextures() {
+        return textures;
+    }
 
     public ArrayList<Entity> getEntities() {
         return entities;
